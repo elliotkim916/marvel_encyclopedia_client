6955d0d23fedb27919bba614010c9710fa103c49
Elliot Kim <elliotkim916@gmail.com>
2018-05-04 17:55:18 -0700

    no longer importing sign-up form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-import './App.css';
 import TopNav from './components/top-nav';
 import InfoSection from './components/info-section';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <TopNav />
-        </header>
-        <InfoSection />
-      </div>
+        <div className="App">
+          <header className="App-header">
+            <TopNav />
+          </header>
+          <main>
+            <InfoSection />
+          </main>
+        </div>
     );
   }
 }
