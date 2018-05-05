5d050ed20e2a67db2e3da1d997d0c6bf72f8d094
Elliot Kim <elliotkim916@gmail.com>
2018-05-05 03:05:48 -0700

    gave a tags className

--- src/components/additional-links.js
+++ src/components/additional-links.js
@@ -11,18 +11,21 @@ export default function AdditionalLinks(props) {
             <a 
                 href="https://marvel.com/characters/1009664/thor/featured?utm_campaign=apiRef&utm_source=fd39c1a1e2bfae2ff6b93140da9570c0" 
                 target="_blank"
+                className="new-link"
                 rel="noopener noreferrer">
                 Detail
             </a>
             <a 
                 href="http://marvel.com/universe/Thor_(Thor_Odinson)?utm_campaign=apiRef&utm_source=fd39c1a1e2bfae2ff6b93140da9570c0" 
                 target="_blank"
+                className="new-link"
                 rel="noopener noreferrer">
                 Wiki
             </a>
             <a 
                 href="https://marvel.com/comics/characters/1009664/thor?utm_campaign=apiRef&utm_source=fd39c1a1e2bfae2ff6b93140da9570c0" 
                 target="_blank"
+                className="new-link"
                 rel="noopener noreferrer">
                 Comics Link
             </a>
