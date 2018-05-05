23cad12b4cce258a223fa25c66fdbf7b4ff3f2e2
Elliot Kim <elliotkim916@gmail.com>
2018-05-04 17:55:56 -0700

    importing BrowserRouter and setting up routes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import SignUp from './components/sign-up';
+import SearchResults from './components/search-results';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 ReactDOM.render(
-    <App />, 
+    <Router>
+        <Switch>
+            <Route exact path='/landing-page' component={App} />
+            <Route exact path='/sign-up' component={SignUp} />
+            <Route exact path='/search-results' component={SearchResults} />
+        </Switch>
+    </Router>, 
     document.getElementById('root')
 );
 
-registerServiceWorker();
+
