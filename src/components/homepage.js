8cbd67fda7777d940c1cd20733a7c1ea69b05962
Elliot Kim <elliotkim916@gmail.com>
2018-05-05 21:29:41 -0700

    commented out NotesList component for now

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import SearchForm from './search-form';
-import NotesList from './notes-list';
+// import NotesList from './notes-list';
 import AlreadyReadList from './already-read-list';
 import ReadLaterList from './read-later-list';
 
@@ -9,7 +9,7 @@ export default function Homepage(props) {
     return (
         <section aria-label="homepage">
             <SearchForm />
-            <NotesList />
+            {/* <NotesList /> */}
             <AlreadyReadList />
             <ReadLaterList />
         </section>
