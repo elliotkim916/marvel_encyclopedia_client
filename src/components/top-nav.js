9e79ec2af67d4564757ad440653e69c561dd13ed
Elliot Kim <elliotkim916@gmail.com>
2018-05-05 21:33:52 -0700

    wrapped all items in section tabs with a className & gave a tags href values

--- src/components/top-nav.js
+++ src/components/top-nav.js
@@ -3,36 +3,38 @@ import './top-nav.css';
 
 export default function TopNav(props) {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <a 
-                        href="#demo"
-                        className="demo"
-                        aria-label="Demo of application"
-                    >
-                    Demo
-                    </a>
-                </li>
-                <li>
-                    <a
-                        href="#sign-up"
-                        className="sign-up"
-                        aria-label="Create a new account"
-                    >
-                    Sign up
-                    </a>    
-                </li>
-                <li>
-                    <a
-                        href="#log-in"
-                        className="log-in"
-                        aria-label="Log in with existing account"
-                    >
-                    Log in
-                    </a>
-                </li>
-            </ul>
-        </nav>
+        <section className="top-nav">
+            <nav>
+                <ul>
+                    <li className="nav-list-item">
+                        <a 
+                            href="homepage"
+                            className="demo"
+                            aria-label="Demo of application"
+                        >
+                        Demo
+                        </a>
+                    </li>
+                    <li className="nav-list-item">
+                        <a
+                            href="sign-up"
+                            className="sign-up"
+                            aria-label="Create a new account"
+                        >
+                        Sign up
+                        </a>    
+                    </li>
+                    <li className="nav-list-item">
+                        <a
+                            href="#log-in"
+                            className="log-in"
+                            aria-label="Log in with existing account"
+                        >
+                        Log in
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </section>
     );
 }
