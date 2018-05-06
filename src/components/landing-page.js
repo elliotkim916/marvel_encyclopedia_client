95d0442298c846b522a98a8ce41fcfc5363dbfb2
Elliot Kim <elliotkim916@gmail.com>
2018-05-05 21:27:21 -0700

    imported top-nav and info-section

---
+++ src/components/landing-page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TopNav from './top-nav';
+import InfoSection from './info-section';
+
+export default function LandingPage(props) {
+    return (
+        <section>
+            <TopNav />
+            <InfoSection />
+        </section>
+    );
+}
