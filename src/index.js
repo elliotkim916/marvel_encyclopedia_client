31efab5c304052e909f0b0429a82f4692d3222c9
Elliot Kim <elliotkim916@gmail.com>
2018-05-06 19:40:09 -0700

    commented out import of createStore

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import {createStore} from 'redux';
+//import {createStore} from 'redux';
 import store from './store';
 import {Provider} from 'react-redux';
 
