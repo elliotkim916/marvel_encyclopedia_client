c10ca3c68633ba8fb8774afa5d02f95534025ac3
Elliot Kim <elliotkim916@gmail.com>
2018-05-06 19:36:22 -0700

    export marvel api base url, endpoint, and public key

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const MARVEL_API_BASE_URL = "https://gateway.marvel.com:443/v1/";
+export const MARVEL_CHARACTERS_ENDPOINT = "public/characters";
+export const PUBLIC_KEY = "fd39c1a1e2bfae2ff6b93140da9570c0";
+
