471ca55161e3c5ee5b74f1e920eb0b0f81a38c43
Elliot Kim <elliotkim916@gmail.com>
2018-05-07 22:20:51 -0700

    using ternary operator and imported new component EventsList

--- src/components/search-results.js
+++ src/components/search-results.js
@@ -1,19 +1,31 @@
 import React from 'react';
-
+import {connect} from 'react-redux';
 import SearchForm from './search-form';
 // import NewNote from './new-note';
 import CharacterInfo from './character-info';
 import ComicsList from './comics-list';
+import EventsList from './events-list';
 import AdditionalLinks from './additional-links';
 
-export default function SearchResults(props) {
+
+export function SearchResults(props) {
+    console.log(props.searchResult);
+
     return (
         <section aria-label="search results">
             <SearchForm />
             {/* <NewNote /> */}
-            <CharacterInfo />
-            <ComicsList />
-            <AdditionalLinks />
+            { props.searchResult.loading ? '' : <CharacterInfo character={props.searchResult}/> }
+            { props.searchResult.loading ? '' : <ComicsList character={props.searchResult}/> }
+            { props.searchResult.loading ? '' : <EventsList character={props.searchResult}/> }
+            { props.searchResult.loading ? '' : <AdditionalLinks character={props.searchResult}/> }  
         </section>
-    );
+        );
+
 }
+
+const mapStateToProps = state => ({
+    searchResult: state.character
+});
+
+export default connect(mapStateToProps)(SearchResults);
