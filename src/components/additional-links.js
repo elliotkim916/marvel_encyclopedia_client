8236a754b2b9ba99e558f3397541643cda7e1143
Elliot Kim <elliotkim916@gmail.com>
2018-05-07 18:37:26 -0700

    applied conditional rendering to successfully show links

--- src/components/additional-links.js
+++ src/components/additional-links.js
@@ -2,33 +2,39 @@ import React from 'react';
 import './additional-links.css';
 
 export default function AdditionalLinks(props) {
+    console.log(props.character.urls);
+    let urls = '';
+    if (props.character.urls) { 
+        urls = props.character.urls.map((link) => link.url);
+    }
+    
     return (
         <section className="additional-links-section">
             <header>
                 <h3>Additional Links</h3>
             </header>
-        
+         
             <a 
-                href="https://marvel.com/characters/1009664/thor/featured?utm_campaign=apiRef&utm_source=fd39c1a1e2bfae2ff6b93140da9570c0" 
+                href={urls[0]} 
                 target="_blank"
                 className="new-link"
                 rel="noopener noreferrer">
                 Detail
             </a>
             <a 
-                href="http://marvel.com/universe/Thor_(Thor_Odinson)?utm_campaign=apiRef&utm_source=fd39c1a1e2bfae2ff6b93140da9570c0" 
+                href={urls[1]}
                 target="_blank"
                 className="new-link"
                 rel="noopener noreferrer">
                 Wiki
             </a>
             <a 
-                href="https://marvel.com/comics/characters/1009664/thor?utm_campaign=apiRef&utm_source=fd39c1a1e2bfae2ff6b93140da9570c0" 
+                href={urls[2]}
                 target="_blank"
                 className="new-link"
                 rel="noopener noreferrer">
                 Comics Link
-            </a>
+            </a> 
         </section>
     );
 }
