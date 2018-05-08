5f2287317d17079435a142e54d5e9657bc086532
Elliot Kim <elliotkim916@gmail.com>
2018-05-07 22:21:24 -0700

    created events list component that details all events searched character is in

---
+++ src/components/events-list.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './events-list.css';
+
+export default function EventsList(props) {
+    let eventTitles = '';
+    if (props.character.events) {
+        eventTitles = props.character.events.items.map((event, index) => (
+            <li key={index} className="event-name">
+                <p className="event-title">{event.name}</p>
+            </li>
+        ))
+    }
+
+    return (
+        <section className="events-list-section">
+            <header>
+                <h2>Comic Events & Crossovers</h2>
+            </header>
+
+            <div className="events-container">
+                <ul>
+                    {eventTitles}
+                </ul>
+            </div>    
+        </section>
+
+    )
+}
