cca9f531abdaf96657d71e6a05fd33238ef1132a
Elliot Kim <elliotkim916@gmail.com>
2018-05-07 18:36:12 -0700

    applied conditional rendering and also rendering total issues available

--- src/components/comics-list.js
+++ src/components/comics-list.js
@@ -1,41 +1,40 @@
 import React from 'react';
-import {connect} from 'react-redux';
 import './comics-list.css';
 
-export function ComicsList(props) {
-    const allComics = props.allComics.map((comic, index) => (
-        <li key={index}>
-            <form className="comics-form">
-                <a href="#comic-searched">
-                    <img 
-                        src={comic.img} 
-                        alt="comic book issue" />
-                    <p className="comic-title">{comic.title}</p>
-                </a>
-                
-                <input type="radio" id="already-read" name="comic" />
-                <label htmlFor="already-read"><span>Already Read</span></label>
-                <input type="radio" id="bookmark" name="comic" />
-                <label htmlFor="bookmark"><span>Read Later</span></label>
-            </form>
-        </li>
-    ));
+export default function ComicsList(props) {
+    console.log(props.character.comics);
+    let comicTitles = '';
+    if (props.character.comics) {
+        console.log(props.character.comics.available);
+        comicTitles = props.character.comics.items.map((comic, index) => (
+            
+            <li key={index}>
+                <form className="comics-form">
+                    <a href="#comic-searched">
+                        <p className="comic-title">{comic.name}</p>
+                    </a>
+                    
+                    <input type="radio" id="already-read" name="comic" />
+                    <label htmlFor="already-read"><span>Already Read</span></label>
+                    <input type="radio" id="bookmark" name="comic" />
+                    <label htmlFor="bookmark"><span>Read Later</span></label>
+                </form>
+            </li>
+        ));
+    }
+    
+    
 
     return (
         <section className="comics-list-section">
             <header>
                 <h3>Comics List</h3>
             </header>
-
+            {props.character.comics ? <h2>Issues Available: {props.character.comics.available}</h2> : ''}
+            
             <ul>
-                {allComics}
+                {comicTitles}
             </ul>
         </section>
     );
 }
-
-const mapStateToProps = state => ({
-    allComics: state.search.allComics
-});
-
-export default connect(mapStateToProps)(ComicsList);
