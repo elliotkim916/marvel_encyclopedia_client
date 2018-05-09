d65b3e48eafc5b729fdcb3f6f0a97f48eb4cdc80
Elliot Kim <elliotkim916@gmail.com>
2018-05-08 17:29:27 -0700

    no longer importing BrowserRouter and put history prop in Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import history from '../history';
 import LandingPage from './landing-page';
 import SignUp from './sign-up';
 import SearchResults from './search-results';
 import Homepage from './homepage';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 
 export default function App(props) {
     return (
-      <Router>
+      <Router history={history}>
         <div className="App">
           <main>
             <Switch>
