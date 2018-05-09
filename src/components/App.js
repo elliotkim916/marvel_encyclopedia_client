0697a4b1c85f8096e7a7ecc9e9846fd6063a7001
Elliot Kim <elliotkim916@gmail.com>
2018-05-08 22:43:55 -0700

    added route to Comic component

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import LandingPage from './landing-page';
 import SignUp from './sign-up';
 import SearchResults from './search-results';
 import Homepage from './homepage';
-
+import Comic from './comic';
 
 export default function App(props) {
     return (
@@ -16,7 +16,8 @@ export default function App(props) {
               <Route exact path="/" component={LandingPage} />
               <Route exact path="/sign-up" component={SignUp} />
               <Route exact path="/homepage" component={Homepage} />
-              <Route exact path ="/search-results" component={SearchResults} />     
+              <Route exact path="/search-results" component={SearchResults} />
+              <Route exact path="/comic" component={Comic} />     
             </Switch> 
           </main>
         </div>
