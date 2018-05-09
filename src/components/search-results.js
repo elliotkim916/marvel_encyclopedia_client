7cb03c7b73e01a3a7585f1ad24254115309c2f8a
Elliot Kim <elliotkim916@gmail.com>
2018-05-08 22:47:42 -0700

    imported Comic component

--- src/components/search-results.js
+++ src/components/search-results.js
@@ -4,18 +4,20 @@ import SearchForm from './search-form';
 // import NewNote from './new-note';
 import CharacterInfo from './character-info';
 import ComicsList from './comics-list';
+import Comic from './comic';
 import EventsList from './events-list';
 import AdditionalLinks from './additional-links';
 
 export function SearchResults(props) {
-    // console.log(props.searchResult);
-
+    //console.log(props.searchResult);
+    console.log(props.comicResult);
     return (
         <section aria-label="search results">
             <SearchForm />
             {/* <NewNote /> */}
             { props.searchResult.loading ? '' : <CharacterInfo character={props.searchResult} /> }
-            { props.searchResult.loading ? '' : <ComicsList character={props.searchResult}/> }
+            { props.searchResult.loading ? '' : <ComicsList character={props.searchResult} /> }
+            { props.comicResult.loading ? '' : <Comic comic={props.searchResult} /> }
             { props.searchResult.loading ? '' : <EventsList character={props.searchResult}/> }
             { props.searchResult.loading ? '' : <AdditionalLinks character={props.searchResult}/> }  
         </section>
@@ -24,7 +26,8 @@ export function SearchResults(props) {
 }
 
 const mapStateToProps = state => ({
-    searchResult: state.character
+    searchResult: state.character.character,
+    comicResult: state.comic.comic
 });
 
 export default connect(mapStateToProps)(SearchResults);
