e4ea49e133bf6de6a192457f946707dd2be181bc
Elliot Kim <elliotkim916@gmail.com>
2018-05-08 19:50:57 -0700

    created comicReducer to handle finding comic action

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,15 @@
 import {
     SEARCH_CHARACTER_REQUEST,
     SEARCH_CHARACTER_SUCCESS,
-    SEARCH_CHARACTER_ERROR
+    SEARCH_CHARACTER_ERROR,
+    FIND_COMIC_REQUEST,
+    FIND_COMIC_SUCCESS,
+    FIND_COMIC_ERROR
 } from '../actions';
 
 const initialState = {
     character: {},
+    comic: {},
     loading: false,
     error: null
 };
@@ -23,6 +27,27 @@ export function characterReducer(state=initialState, action) {
             error: null
         });
     } else if (action.type === SEARCH_CHARACTER_ERROR) {
+        return Object.assign({}, state, {
+            error: action.error,
+            loading: false
+        });
+    }
+    return state;
+}
+
+export function comicReducer(state=initialState, action) {
+    if (action.type === FIND_COMIC_REQUEST) {
+        return Object.assign({}, state, {
+            loading: true,
+            error: null
+        });
+    } else if (action.type === FIND_COMIC_SUCCESS) {
+        return Object.assign({}, state, {
+            comic: action.comic,
+            loading: false,
+            error: null
+        });
+    } else if (action.type === FIND_COMIC_ERROR) {
         return Object.assign({}, state, {
             error: action.error,
             loading: false
