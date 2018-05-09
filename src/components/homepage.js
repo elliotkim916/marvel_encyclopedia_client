5737925e144c946c58ac4a2ceeab46775120a2f7
Elliot Kim <elliotkim916@gmail.com>
2018-05-08 17:30:53 -0700

    commented out unnecessary imports

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -2,16 +2,16 @@ import React from 'react';
 
 import SearchForm from './search-form';
 // import NotesList from './notes-list';
-import AlreadyReadList from './already-read-list';
-import ReadLaterList from './read-later-list';
+// import AlreadyReadList from './already-read-list';
+// import ReadLaterList from './read-later-list';
 
 export default function Homepage(props) {
     return (
         <section aria-label="homepage">
             <SearchForm />
             {/* <NotesList /> */}
-            <AlreadyReadList />
-            <ReadLaterList />
+            {/* <AlreadyReadList />
+            <ReadLaterList /> */}
         </section>
     );
 }
