5b6b0306c8e1817c4de5d973884c5bfd30f6ccc4
Elliot Kim <elliotkim916@gmail.com>
2018-05-10 10:36:08 -0700

    added dispatch findEvent action on li item when clicked on

--- src/components/events-list.js
+++ src/components/events-list.js
@@ -1,27 +1,38 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import './events-list.css';
+import {findEvent} from '../actions';
 
-export default function EventsList(props) {
-    let eventTitles = '';
-    if (props.character.events) {
-        eventTitles = props.character.events.items.map((event, index) => (
-            <li key={`event-${index}`} className="event-name">
-                <p className="event-title">{event.name}</p>
-            </li>
-        ))
+export class EventsList extends React.Component {
+    render() {
+        let eventTitles = '';
+        if (this.props.character.events) {
+            console.log(this.props.character.events);
+            eventTitles = this.props.character.events.items.map((event, index) => (
+                <li key={`event-${index}`} className="event-name" onClick={() => this.props.dispatch(findEvent(event.resourceURI))}>
+                    <p className="event-title">{event.name}</p>
+                </li>
+            ))
+        }
+
+        return (
+            <section className="events-list-section">
+                <header>
+                    <h2>Comic Events & Crossovers</h2>
+                </header>
+
+                <div className="events-container">
+                    <ul>
+                        {eventTitles}
+                    </ul>
+                </div>    
+            </section>
+        );
     }
-
-    return (
-        <section className="events-list-section">
-            <header>
-                <h2>Comic Events & Crossovers</h2>
-            </header>
-
-            <div className="events-container">
-                <ul>
-                    {eventTitles}
-                </ul>
-            </div>    
-        </section>
-    );
 }
+
+const mapStateToProps = state => ({
+    eventData: state.event.event
+});
+
+export default connect(mapStateToProps)(EventsList);
