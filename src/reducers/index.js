b781c2d26823cf01676013dc99ed55b011592472
Elliot Kim <elliotkim916@gmail.com>
2018-05-10 10:27:44 -0700

    added reducers to handle find event actions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,12 +4,16 @@ import {
     SEARCH_CHARACTER_ERROR,
     FIND_COMIC_REQUEST,
     FIND_COMIC_SUCCESS,
-    FIND_COMIC_ERROR
+    FIND_COMIC_ERROR,
+    FIND_EVENT_REQUEST,
+    FIND_EVENT_SUCCESS,
+    FIND_EVENT_ERROR
 } from '../actions';
 
 const initialState = {
     character: {},
     comic: {},
+    event: {},
     loading: false,
     error: null
 };
@@ -48,6 +52,27 @@ export function comicReducer(state=initialState, action) {
             error: null
         });
     } else if (action.type === FIND_COMIC_ERROR) {
+        return Object.assign({}, state, {
+            error: action.error,
+            loading: false
+        });
+    }
+    return state;
+}
+
+export function eventReducer(state=initialState, action) {
+    if (action.type === FIND_EVENT_REQUEST) {
+        return Object.assign({}, state, {
+            loading: true,
+            error: null
+        });
+    } else if (action.type === FIND_EVENT_SUCCESS) {
+        return Object.assign({}, state, {
+            event: action.event,
+            loading: false,
+            error: null
+        });
+    } else if (action.type === FIND_EVENT_ERROR) {
         return Object.assign({}, state, {
             error: action.error,
             loading: false
