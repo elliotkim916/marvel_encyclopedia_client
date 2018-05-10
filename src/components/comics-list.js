caec0a6298e15dc26d6ded327563297e37c3fcc8
Elliot Kim <elliotkim916@gmail.com>
2018-05-09 18:45:29 -0700

    altered input id and label htmlFor attribute along list key attribute

--- src/components/comics-list.js
+++ src/components/comics-list.js
@@ -5,41 +5,58 @@ import {findComic} from '../actions';
 
 export class ComicsList extends React.Component {
     render() {
-    let comicTitles = '';
-    if (this.props.character.comics) {
-        comicTitles = this.props.character.comics.items.map((comic, index) => (
-            <li key={index}>
-                <form className="comics-form">
-                    <a href="#comic-searched" onClick={this.props.dispatch(findComic(comic.resourceURI))}>
-                        <p className="comic-title">{comic.name}</p>
-                    </a>
-                    
-                    <input type="radio" id="already-read" name="comic" />
-                    <label htmlFor="already-read"><span>Already Read</span></label>
-                    <input type="radio" id="bookmark" name="comic" />
-                    <label htmlFor="bookmark"><span>Read Later</span></label>
-                </form>
-            </li>
-        ));
+        let comicTitles = '';
+        if (this.props.character.comics) {
+            comicTitles = this.props.character.comics.items.map((comic, index) => (
+                <li key={`comic-${index}`}>
+                    <form className="comics-form">
+                        <h3 className="comic-title" onClick={() => this.props.dispatch(findComic(comic.resourceURI))}>{comic.name}</h3>
+                        <input 
+                            type="radio" 
+                            id={`already-read-${index}`} 
+                            name={`comic-${index}`}
+                            className="already-read-input" 
+                        />
+                        <label 
+                            htmlFor={`already-read-${index}`}
+                            className="already-read-label"
+                        >
+                            <span>Already Read</span>
+                        </label>
+                        <input 
+                            type="radio" 
+                            id={`bookmark-${index}`} 
+                            name={`comic-${index}`}
+                            className="read-later-input" 
+                        />
+                        <label 
+                            htmlFor={`bookmark-${index}`}
+                            className="read-later-label"
+                        >
+                            <span>Read Later</span>
+                        </label>
+                    </form>
+                </li>
+            ));
+        }
+        
+        return (
+            <section className="comics-list-section">
+                <header>
+                    <h2>Comics</h2>
+                </header>
+                {this.props.character.comics ? <h2>Issues Available: {this.props.character.comics.available}</h2> : ''}
+                
+                <ul>
+                    {comicTitles}
+                </ul>
+            </section>
+        );
     }
-    
-    return (
-        <section className="comics-list-section">
-            <header>
-                <h2>Comics</h2>
-            </header>
-            {this.props.character.comics ? <h2>Issues Available: {this.props.character.comics.available}</h2> : ''}
-            
-            <ul>
-                {comicTitles}
-            </ul>
-        </section>
-    );
-}
 }
 
 const mapStateToProps = state => ({
-    comicData: state.comic.data
+    comicData: state.comic.comic
 });
 
 export default connect(mapStateToProps)(ComicsList);
