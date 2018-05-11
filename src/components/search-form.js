f0c69f9a917777f4a638d5d7c75d3efc43d6988c
Elliot Kim <elliotkim916@gmail.com>
2018-05-11 09:07:02 -0700

    got rid of mapStateToProps and only connected dispatch to SearchForm

--- src/components/search-form.js
+++ src/components/search-form.js
@@ -18,17 +18,27 @@ export class SearchForm extends React.Component {
                     <h1>Marvel Encyclopedia</h1>
                 </header>
 
-                <form className="search-form" onSubmit={(e) => this.onSubmit(e)}>
-                    <input type="text" placeholder="e.g. Thor" name="search-character" className="search-input" ref={input => this.input = input} />
-                    <button type="submit" className="search-btn">Search</button>
+                <form 
+                    className="search-form" 
+                    onSubmit={(e) => this.onSubmit(e)}
+                >
+                    <input 
+                        type="text" 
+                        placeholder="e.g. Thor" 
+                        name="search-character" 
+                        className="search-input" 
+                        ref={input => this.input = input} 
+                    />
+                    <button 
+                        type="submit" 
+                        className="search-btn"
+                    >
+                    Search
+                    </button>
                 </form>
         </section>
         );
     }
 }
 
-const mapStateToProps = state => ({
-    data: state.character.data,
-});
-
-export default connect(mapStateToProps)(SearchForm);
+export default connect()(SearchForm);
