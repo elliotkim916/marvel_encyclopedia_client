417e489976b4af0da9f6ce013492debacdec4d49
Elliot Kim <elliotkim916@gmail.com>
2018-05-14 15:50:34 -0700

    added link to LogIn component

--- src/components/sign-up.js
+++ src/components/sign-up.js
@@ -5,17 +5,19 @@ export default function SignUp(props) {
     return (
         <section id="sign-up">
             <header>
-                <h3>Being your Marvel journey now!</h3>
+                <h2>Marvel Encyclopedia</h2>
             </header>
 
-            <form class="signup-form">
+            <form className="signup-form">
+                <h3>Begin your Marvel journey now!</h3>
                 <div>
                     <input type="text" placeholder="Enter username" name="username" />
                 </div>
                 <div>
                     <input type="password" placeholder="Enter password" name="password" />
                 </div>
-                <button type="submit">Sign Up</button>
+                <button type="submit">Sign Up</button><br />
+                <span>Already have an account? <a href="/log-in">Log in here</a></span>
             </form>
       </section>
     )
