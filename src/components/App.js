0667602c349cb78d6ad755c05e03dde2f27f7718
Elliot Kim <elliotkim916@gmail.com>
2018-05-14 15:48:18 -0700

    created route to LogIn component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { Router, Route, Switch } from 'react-router-dom';
 import history from '../history';
 import LandingPage from './landing-page';
 import SignUp from './sign-up';
+import LogIn from './log-in';
 import SearchResults from './search-results';
 import Homepage from './homepage';
 import Comic from './comic';
@@ -16,6 +17,7 @@ export default function App(props) {
             <Switch>
               <Route exact path="/" component={LandingPage} />
               <Route exact path="/sign-up" component={SignUp} />
+              <Route exact path="/log-in" component={LogIn} />
               <Route exact path="/homepage" component={Homepage} />
               <Route exact path="/search-results" component={SearchResults} />
               <Route exact path="/event" component={Event} />  
