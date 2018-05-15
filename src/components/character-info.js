16266178de675d2c237bb35485c2799927d36233
Elliot Kim <elliotkim916@gmail.com>
2018-05-15 10:41:25 -0700

    added Read More link

--- src/components/character-info.js
+++ src/components/character-info.js
@@ -8,14 +8,28 @@ export default class CharacterInfo extends React.Component {
             imgUrl = this.props.character.thumbnail.path + '/portrait_uncanny.' + this.props.character.thumbnail.extension;
         }
         
+        let urls = '';
+        if (this.props.character.urls) { 
+            urls = this.props.character.urls.map((link) => link.url);
+        }
+
         return (
             <section className="character-info-section">
+            <div>
                 <img 
                     src={imgUrl}
                     alt="searched character" 
                     className="searched-img" />
                 <h2>{this.props.character.name}</h2>
                 <p>{this.props.character.description}</p>
+                <a 
+                    href={urls[1]}
+                    target="_blank"
+                    className="info-new-link"
+                    rel="noopener noreferrer">
+                    Read More >>
+                </a>
+            </div>
             </section>
         );
     }
