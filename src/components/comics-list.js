b5298af9732c44cc8bafee21e4c3821c5b200490
Elliot Kim <elliotkim916@gmail.com>
2018-05-15 10:44:54 -0700

    added link to See More, changed header to h2, & put radio buttons in div

--- src/components/comics-list.js
+++ src/components/comics-list.js
@@ -5,43 +5,60 @@ import {findComic} from '../actions';
 
 export class ComicsList extends React.Component {
     render() {
+        let urls = '';
+
+        if (this.props.character.urls) { 
+            urls = this.props.character.urls.map((link) => link.url);
+        }
+
         let comicTitles = '';
-        if (this.props.character.comics) {
-            comicTitles = this.props.character.comics.items.map((comic, index) => (
+
+        if (this.props.comic) {
+            comicTitles = this.props.comic.map((comic, index) => (
                 <li key={`comic-${index}`}>
                     <form className="comics-form">
+                        <img 
+                            src={`${comic.thumbnail.path}/portrait_fantastic.${comic.thumbnail.extension}`} 
+                            alt="Comic book cover"
+                            className="comic-cover-img"
+                            onClick={() => this.props.dispatch(findComic(comic.resourceURI))} 
+                        />
                         <h3 
                             className="comic-title" 
                             onClick={() => this.props.dispatch(findComic(comic.resourceURI))}
                         >
-                        {comic.name}
+                        {comic.title}
                         </h3>
+                        <div className="radio-btns">
+                            <input 
+                                type="radio" 
+                                id={`already-read-${index}`} 
+                                name={`comic-${index}`}
+                                className="already-read-input"
+                                value={comic.title} 
+                                // onChange ={() =>}
+                            />
+                            <label 
+                                htmlFor={`already-read-${index}`}
+                                className="already-read-label"
+                            >
+                                <span>Already Read</span>
+                            </label>
 
-                        <input 
-                            type="radio" 
-                            id={`already-read-${index}`} 
-                            name={`comic-${index}`}
-                            className="already-read-input" 
-                        />
-                        <label 
-                            htmlFor={`already-read-${index}`}
-                            className="already-read-label"
-                        >
-                            <span>Already Read</span>
-                        </label>
-
-                        <input 
-                            type="radio" 
-                            id={`bookmark-${index}`} 
-                            name={`comic-${index}`}
-                            className="read-later-input" 
-                        />
-                        <label 
-                            htmlFor={`bookmark-${index}`}
-                            className="read-later-label"
-                        >
-                            <span>Read Later</span>
-                        </label>
+                            <input 
+                                type="radio" 
+                                id={`bookmark-${index}`} 
+                                name={`comic-${index}`}
+                                className="read-later-input"
+                                value={comic.title}  
+                            />
+                            <label 
+                                htmlFor={`bookmark-${index}`}
+                                className="read-later-label"
+                            >
+                                <span>Read Later</span>
+                            </label>
+                        </div>
                     </form>
                 </li>
             ));
@@ -50,13 +67,20 @@ export class ComicsList extends React.Component {
         return (
             <section className="comics-list-section">
                 <header>
-                    <h2>Comics</h2>
+                    <h2>Comic Book Appearances</h2>
                 </header>
                 {this.props.character.comics ? <h2>Issues Available: {this.props.character.comics.available}</h2> : ''}
                 
                 <ul>
                     {comicTitles}
                 </ul>
+                <a 
+                    href={urls[2]}
+                    target="_blank"
+                    className="comics-new-link"
+                    rel="noopener noreferrer">
+                    See More >>
+                </a> 
             </section>
         );
     }
