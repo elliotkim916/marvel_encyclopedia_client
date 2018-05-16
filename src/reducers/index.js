14e583fc14fc4b0bbde68731f08907c0d3ceb77d
Elliot Kim <elliotkim916@gmail.com>
2018-05-16 09:18:12 -0700

    established reducers for searchCharacterComic & searchCharacterEvent actions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,16 +7,66 @@ import {
     FIND_COMIC_ERROR,
     FIND_EVENT_REQUEST,
     FIND_EVENT_SUCCESS,
-    FIND_EVENT_ERROR
+    FIND_EVENT_ERROR,
+    SEARCH_CHARACTER_COMIC_REQUEST,
+    SEARCH_CHARACTER_COMIC_SUCCESS,
+    SEARCH_CHARACTER_COMIC_ERROR,
+    SEARCH_CHARACTER_EVENT_REQUEST,
+    SEARCH_CHARACTER_EVENT_SUCCESS,
+    SEARCH_CHARACTER_EVENT_ERROR
 } from '../actions';
 
 const initialState = {
     searchedCharacter: {},
+    searchedCharacterComic: [],
+    searchedCharacterEvent: [],
     clickedComic: {},
     clickedEvent: {},
     loading: false,
     error: null
 };
+
+export function characterEventReducer(state=initialState, action) {
+    if (action.type === SEARCH_CHARACTER_EVENT_REQUEST) {
+        return Object.assign({}, state, {
+            loading: true,
+            error: null
+        });
+    } else if (action.type === SEARCH_CHARACTER_EVENT_SUCCESS) {
+        return Object.assign({}, state, {
+            searchedCharacterEvent: action.characterEvent,
+            loading: false,
+            error: null
+        });
+    } else if (action.type === SEARCH_CHARACTER_EVENT_ERROR) {
+        return Object.assign({}, state, {
+            error: action.error,
+            loading: false
+        });
+    }
+    return state;
+}
+
+export function characterComicReducer(state=initialState, action) {
+    if (action.type === SEARCH_CHARACTER_COMIC_REQUEST) {
+        return Object.assign({}, state, {
+            loading: true,
+            error: null
+        });
+    } else if (action.type === SEARCH_CHARACTER_COMIC_SUCCESS) {
+        return Object.assign({}, state, {
+            searchedCharacterComic: action.characterComic,
+            loading: false,
+            error: null
+        });
+    } else if (action.type === SEARCH_CHARACTER_COMIC_ERROR) {
+        return Object.assign({}, state, {
+            error: action.error,
+            loading: false
+        });
+    }
+    return state;
+}
 
 export function characterReducer(state=initialState, action) {
     if (action.type === SEARCH_CHARACTER_REQUEST) {
