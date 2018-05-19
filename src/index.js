209f68c402c8ca3c4491dba1c741917e78cb8c47
Elliot Kim <elliotkim916@gmail.com>
2018-05-18 20:18:45 -0700

    cleaned up code and changed order of imports

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-//import {createStore} from 'redux';
-import store from './store';
 import {Provider} from 'react-redux';
-
-// const store = createStore(state => state, initialState);
+import App from './components/App';
+import store from './store';
+import './index.css';
 
 ReactDOM.render(
     <Provider store={store}>
