6665db40491e5f41a8eb1e8b93e9bc97d110ba86
Elliot Kim <elliotkim916@gmail.com>
2018-05-19 16:52:30 -0700

    estabalished LoginPage which redirects user to /dashboard if logged in

---
+++ src/components/login-page.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {Link, Redirect} from 'react-router-dom';
+
+import LoginForm from './login-form';
+
+export function LoginPage(props) {
+    if (props.loggedIn) {
+        return <Redirect to="/dashboard" />;
+    }
+
+    return (
+        <div className="login-page">
+            <header>
+                <h2><Link to="/">Marvel Encyclopedia</Link></h2>
+            </header>
+            <LoginForm />
+        </div>
+    )
+}
+
+const mapStateToProps = state => ({
+    loggedIn: state.auth.currentUser !== null
+});
+
+export default connect(mapStateToProps)(LoginPage);
