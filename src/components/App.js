d9de10d72c5ead4b47ecda1fa9e417240836a301
Elliot Kim <elliotkim916@gmail.com>
2018-05-19 15:55:35 -0700

    dispatches refreshAuthToken action every hour if user logs in periodically and does not when user logs out

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,53 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import { Router, Route, Switch } from 'react-router-dom';
 import history from '../history';
+import Dashboard from './dashboard';
+import RegistrationPage from './registration-page';
 import LandingPage from './landing-page';
-import SignUp from './sign-up';
-import LogIn from './log-in';
+import LoginPage from './login-page';
 import SearchResults from './search-results';
-import Homepage from './homepage';
 import Comic from './comic';
 import Event from './event';
+import {refreshAuthToken} from '../actions/auth';
 
-export default function App(props) {
+export class App extends React.Component {
+  componentDidUpdate(prevProps) {
+    if (!prevProps.loggedIn && this.props.loggedIn) {
+      this.startPeriodicRefresh();
+    } else if (prevProps.loggedIn && !this.props.loggedIn) {
+      this.stopPeriodicRefresh();
+    }
+  }
+
+  componentWillUnmount() {
+    this.stopPeriodicRefresh();
+  }
+
+  startPeriodicRefresh() {
+    this.refreshInterval = setInterval(() => 
+      this.props.dispatch(refreshAuthToken()),
+      60 * 60 * 100
+    )
+  }
+
+  stopPeriodicRefresh() {
+    if (!this.refreshInterval) {
+      return;
+    }
+    clearInterval(this.refreshInterval);
+  }
+
+  render() {
     return (
       <Router history={history}>
         <div className="App">
           <main>
             <Switch>
               <Route exact path="/" component={LandingPage} />
-              <Route exact path="/sign-up" component={SignUp} />
-              <Route exact path="/log-in" component={LogIn} />
-              <Route exact path="/homepage" component={Homepage} />
+              <Route exact path="/register" component={RegistrationPage} />
+              <Route exact path="/login" component={LoginPage} />
+              <Route exact path="/dashboard" component={Dashboard} />
               <Route exact path="/search-results" component={SearchResults} />
               <Route exact path="/event" component={Event} />  
               <Route exact path="/comic" component={Comic} />   
@@ -28,4 +57,11 @@ export default function App(props) {
       </Router>
     );
   }
+}
 
+const mapStateToProps = state => ({
+  hasAuthToken: state.auth.authToken !== null,
+  loggedIn: state.auth.currentUser !== null
+});
+
+export default connect(mapStateToProps)(App);
