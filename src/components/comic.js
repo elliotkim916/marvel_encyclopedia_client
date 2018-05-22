8ef49e136b370497a753b2fd01658df783b6570a
Elliot Kim <elliotkim916@gmail.com>
2018-05-22 00:26:47 -0700

    changed to class and added logout and home buttons at top of page

--- src/components/comic.js
+++ src/components/comic.js
@@ -1,94 +1,116 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import './comic.css';
+import {clearAuth} from '../actions/auth';
+import {clearAuthToken} from '../local-storage';
 import {searchCharacter} from '../actions';
+import './comic.css';
 
-export function Comic(props) {
-    let title = '';
-    let issueNumber = '';
-    let pageCount = '';
-    let description = '';
-    let imgUrl = '';
-    let urls = '';
-    let character = '';
-    let creator = '';
-
-    if (props.comicResult) {
-        title = props.comicResult.title;
-        issueNumber = props.comicResult.issueNumber;
-        pageCount = props.comicResult.pageCount;
-        description = props.comicResult.description;
-       
-        if (props.comicResult.thumbnail) {
-            imgUrl = props.comicResult.thumbnail.path.slice(5) + '/portrait_uncanny.' + props.comicResult.thumbnail.extension;
-        }
-       
-        if (props.comicResult.urls) { 
-            urls = props.comicResult.urls.map((link, index) => link.url);
-        }
-
-        if (props.comicCharacter) {
-            character = props.comicCharacter.map((character, index) => 
-                <li 
-                    key={index} 
-                    onClick={() => props.dispatch(searchCharacter(character.name))} 
-                    className="character-name"
-                >
-                    <img 
-                        src={`${character.thumbnail.path.slice(5)}/portrait_fantastic.${character.thumbnail.extension}`} 
-                        alt="character cover"
-                    /><br/>    
-                    {character.name}
-                </li>
-            );
-        }
-
-        if (props.comicResult.creators) {
-            creator = props.comicResult.creators.items.map((person, index) => 
-                <li key={index}>
-                    <span>{person.name}, {person.role.charAt(0).toUpperCase() + person.role.slice(1)}</span>
-                </li>
-            );
-        }
+export class Comic extends React.Component {
+    logOut() {
+        this.props.dispatch(clearAuth());
+        clearAuthToken();
     }
-      
-    return (
-        <section className="comic-section">
-            <header>
-                <img src={imgUrl} alt="Comic book cover"/>
-                <h2>{title}</h2>
-                <h3><span className="issue-number">Issue Number: {issueNumber}</span><span className="total-pages">Total Pages: {pageCount}</span></h3>
-            </header>
 
-            <div className="comic-description">
-                <div dangerouslySetInnerHTML={{__html:description}} className="description-container"></div>
-                {/* <div className="description-container">{description}</div> */}
-                <div className="character-container">
-                    <h2 className="container-header">Characters In This Issue</h2>
-                        <ul className="character-list">
-                            {character}
-                        </ul>
-                </div>
+    render() {
+        let title = '';
+        let issueNumber = '';
+        let pageCount = '';
+        let description = '';
+        let imgUrl = '';
+        let urls = '';
+        let character = '';
+        let creator = '';
 
-                <div className="creator-container">
-                    <h2 className="container-header">Creators</h2>
-                        <ul>
-                            {creator}
-                        </ul>
-                </div>
-            </div>
+        if (this.props.comicResult) {
+            title = this.props.comicResult.title;
+            issueNumber = this.props.comicResult.issueNumber;
+            pageCount = this.props.comicResult.pageCount;
+            description = this.props.comicResult.description;
+        
+            if (this.props.comicResult.thumbnail) {
+                imgUrl = this.props.comicResult.thumbnail.path.slice(5) + '/portrait_uncanny.' + this.props.comicResult.thumbnail.extension;
+            }
+        
+            if (this.props.comicResult.urls) { 
+                urls = this.props.comicResult.urls.map((link, index) => link.url);
+            }
 
-            <div className="comic-links">
+            if (this.props.comicCharacter) {
+                character = this.props.comicCharacter.map((character, index) => 
+                    <li 
+                        key={index} 
+                        onClick={() => this.props.dispatch(searchCharacter(character.name))} 
+                        className="character-name"
+                    >
+                        <img 
+                            src={`${character.thumbnail.path.slice(5)}/portrait_fantastic.${character.thumbnail.extension}`} 
+                            alt="character cover"
+                        /><br/>    
+                        {character.name}
+                    </li>
+                );
+            }
+
+            if (this.props.comicResult.creators) {
+                creator = this.props.comicResult.creators.items.map((person, index) => 
+                    <li key={index}>
+                        <span>{person.name}, {person.role.charAt(0).toUpperCase() + person.role.slice(1)}</span>
+                    </li>
+                );
+            }
+        }
+        
+        return (
+            <section className="comic-section">
                 <a 
-                    href={urls[0]} 
-                    target="_blank"
-                    className="new-comic-link"
-                    rel="noopener noreferrer">
-                    Purchase Comic >>
+                    href="dashboard"
+                    className="back-home-from-comic"
+                >
+                Home
                 </a>
-            </div>         
-        </section>
-    );
+                <a 
+                    href="login"
+                    className="log-out-from-comic"
+                    onClick={() => this.logOut}
+                >
+                Log Out
+                </a>
+                <header>
+                    <img src={imgUrl} alt="Comic book cover"/>
+                    <h2>{title}</h2>
+                    <h3><span className="issue-number">Issue Number: {issueNumber}</span><span className="total-pages">Total Pages: {pageCount}</span></h3>
+                </header>
+
+                <div className="comic-description">
+                    <div dangerouslySetInnerHTML={{__html:description}} className="description-container"></div>
+                    {/* <div className="description-container">{description}</div> */}
+                    <div className="character-container">
+                        <h2 className="container-header">Characters In This Issue</h2>
+                            <ul className="character-list">
+                                {character}
+                            </ul>
+                    </div>
+
+                    <div className="creator-container">
+                        <h2 className="container-header">Creators</h2>
+                            <ul>
+                                {creator}
+                            </ul>
+                    </div>
+                </div>
+
+                <div className="comic-links">
+                    <a 
+                        href={urls[0]} 
+                        target="_blank"
+                        className="new-comic-link"
+                        rel="noopener noreferrer">
+                        Purchase Comic >>
+                    </a>
+                </div>         
+            </section>
+        );
+    }
 }
 
 const mapStateToProps = state => ({
--- src/components/event.js
+++ src/components/event.js
@@ -2,165 +2,186 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {searchCharacter} from '../actions';
 import {findComic} from '../actions';
-import './event.css';
+import {clearAuth} from '../actions/auth';
+import {clearAuthToken} from '../local-storage';
 import {addData} from '../actions/protected-data';
+import './event.css';
 
-export function Event(props) {
-    let imgUrl = '';
-    let urls = '';
-    let character = '';
-    let comic = '';
-    let creator = '';
-
-    if (props.eventResult.thumbnail) {
-        imgUrl = props.eventResult.thumbnail.path.slice(5) + '/portrait_uncanny.' + props.eventResult.thumbnail.extension;
+export class Event extends React.Component {
+    logOut() {
+        this.props.dispatch(clearAuth());
+        clearAuthToken();
     }
 
-    if (props.eventResult.urls) {
-        urls = props.eventResult.urls.map((link, index) => link.url);
-    }
+    render() {
+        let imgUrl = '';
+        let urls = '';
+        let character = '';
+        let comic = '';
+        let creator = '';
 
-    if (props.eventCharacter) {
-        character = props.eventCharacter.map((character, index) => 
-            <li 
-                key={index} 
-                onClick={() => props.dispatch(searchCharacter(character.name))}
-                className="event-character-name"
-            >
-                <img 
-                    src={`${character.thumbnail.path.slice(5)}/portrait_fantastic.${character.thumbnail.extension}`} 
-                    alt="Character cover"
-                /><br />
-                {character.name}
-            </li>
-        );
-    }
+        if (this.props.eventResult.thumbnail) {
+            imgUrl = this.props.eventResult.thumbnail.path.slice(5) + '/portrait_uncanny.' + this.props.eventResult.thumbnail.extension;
+        }
 
-    if (props.eventComic) {
-        comic = props.eventComic.map((comic, index) => 
-            <li 
-                key={index} 
-                className="event-comic-name"    
-            >
-                <form className="event-comics-form">
-                    <div
-                        className="comic-container" 
-                        onClick={() => props.dispatch(findComic(comic.resourceURI))}
-                    >
-                        <img 
-                            src={`${comic.thumbnail.path.slice(5)}/portrait_fantastic.${comic.thumbnail.extension}`} 
-                            alt="Comic book cover"
-                            className="comic-cover-img"
-                        />
-                        <h3 className="comic-title">
-                        {comic.title}
-                        </h3>
-                    </div>
-                    <div className="radio-btns">
-                        <input 
-                            type="radio" 
-                            id={`already-read-${index}`} 
-                            name={`comic-${index}`}
-                            className="already-read-input"
-                            value={comic.title} 
-                            onChange ={() => props.dispatch(addData(
-                                comic.title, 
-                                'Already Read', 
-                                comic.thumbnail.path + '/portrait_fantastic.' + comic.thumbnail.extension,
-                                comic.resourceURI,
-                                props.loggedIn.username
-                            ))}
-                        />
-                        <label 
-                            htmlFor={`already-read-${index}`}
-                            className="already-read-label"
+        if (this.props.eventResult.urls) {
+            urls = this.props.eventResult.urls.map((link, index) => link.url);
+        }
+
+        if (this.props.eventCharacter) {
+            character = this.props.eventCharacter.map((character, index) => 
+                <li 
+                    key={index} 
+                    onClick={() => this.props.dispatch(searchCharacter(character.name))}
+                    className="event-character-name"
+                >
+                    <img 
+                        src={`${character.thumbnail.path.slice(5)}/portrait_fantastic.${character.thumbnail.extension}`} 
+                        alt="Character cover"
+                    /><br />
+                    {character.name}
+                </li>
+            );
+        }
+
+        if (this.props.eventComic) {
+            comic = this.props.eventComic.map((comic, index) => 
+                <li 
+                    key={index} 
+                    className="event-comic-name"    
+                >
+                    <form className="event-comics-form">
+                        <div
+                            className="comic-container" 
+                            onClick={() => this.props.dispatch(findComic(comic.resourceURI))}
                         >
-                            <span>Already Read</span>
-                        </label>
+                            <img 
+                                src={`${comic.thumbnail.path.slice(5)}/portrait_fantastic.${comic.thumbnail.extension}`} 
+                                alt="Comic book cover"
+                                className="comic-cover-img"
+                            />
+                            <h3 className="comic-title">
+                            {comic.title}
+                            </h3>
+                        </div>
+                        <div className="radio-btns">
+                            <input 
+                                type="radio" 
+                                id={`already-read-${index}`} 
+                                name={`comic-${index}`}
+                                className="already-read-input"
+                                value={comic.title} 
+                                onChange ={() => this.props.dispatch(addData(
+                                    comic.title, 
+                                    'Already Read', 
+                                    comic.thumbnail.path + '/portrait_fantastic.' + comic.thumbnail.extension,
+                                    comic.resourceURI,
+                                    this.props.loggedIn.username
+                                ))}
+                            />
+                            <label 
+                                htmlFor={`already-read-${index}`}
+                                className="already-read-label"
+                            >
+                                <span>Already Read</span>
+                            </label>
 
-                        <input 
-                            type="radio" 
-                            id={`bookmark-${index}`} 
-                            name={`comic-${index}`}
-                            className="read-later-input"
-                            value={comic.title} 
-                            onChange ={() => props.dispatch(addData(
-                                comic.title, 
-                                'Read Later', 
-                                comic.thumbnail.path + '/portrait_fantastic.' + comic.thumbnail.extension,
-                                comic.resourceURI,
-                                props.loggedIn.username
-                            ))} 
-                        />
-                        <label 
-                            htmlFor={`bookmark-${index}`}
-                            className="read-later-label"
-                        >
-                            <span>Read Later</span>
-                        </label>
-                    </div>
-                </form>
-            </li>
-        );
-    }
+                            <input 
+                                type="radio" 
+                                id={`bookmark-${index}`} 
+                                name={`comic-${index}`}
+                                className="read-later-input"
+                                value={comic.title} 
+                                onChange ={() => this.props.dispatch(addData(
+                                    comic.title, 
+                                    'Read Later', 
+                                    comic.thumbnail.path + '/portrait_fantastic.' + comic.thumbnail.extension,
+                                    comic.resourceURI,
+                                    this.props.loggedIn.username
+                                ))} 
+                            />
+                            <label 
+                                htmlFor={`bookmark-${index}`}
+                                className="read-later-label"
+                            >
+                                <span>Read Later</span>
+                            </label>
+                        </div>
+                    </form>
+                </li>
+            );
+        }
 
-    if (props.eventResult.creators) {
-        creator = props.eventResult.creators.items.map((creator, index) => 
-            <li key={index}>
-                <span>{creator.name}, {creator.role.charAt(0).toUpperCase() + creator.role.slice(1)}</span>
-            </li>
-        );
-    }
+        if (this.props.eventResult.creators) {
+            creator = this.props.eventResult.creators.items.map((creator, index) => 
+                <li key={index}>
+                    <span>{creator.name}, {creator.role.charAt(0).toUpperCase() + creator.role.slice(1)}</span>
+                </li>
+            );
+        }
 
-    return (
-        <section className="event-section">
-            <header>
-                <img src={imgUrl} alt="Event cover" />
-                <h2>{props.eventResult.title}</h2>
-            </header>
-
-            <div className="event-description">
-                <p>{props.eventResult.description}</p><br />
+        return (
+            <section className="event-section">
                 <a 
-                    href={urls[1]} 
-                    target="_blank"
-                    className="new-event-link"
-                    rel="noopener noreferrer">
-                    Read More >>
+                    href="dashboard"
+                    className="back-home-from-comic"
+                >
+                Home
                 </a>
-            </div>
+                <a 
+                    href="login"
+                    className="log-out-from-comic"
+                    onClick={() => this.logOut}
+                >
+                Log Out
+                </a>
+                <header>
+                    <img src={imgUrl} alt="Event cover" />
+                    <h2>{this.props.eventResult.title}</h2>
+                </header>
 
-            <div className="event-character-container">
-                <h2 className="event-container-header">Characters In This Event</h2>
-                    <ul className="event-character-list">
-                        {character}
-                    </ul>
-            </div>
-
-            <div className="event-comic-container">
-                <h2 className="event-container-header">{props.eventResult.title} Core Issues & Tie-In Issues</h2>
-                    <ul className="event-comic-list">
-                        {comic}
-                    </ul>
+                <div className="event-description">
+                    <p>{this.props.eventResult.description}</p><br />
                     <a 
-                        href={urls[0]} 
+                        href={urls[1]} 
                         target="_blank"
                         className="new-event-link"
                         rel="noopener noreferrer">
-                        See More >>
+                        Read More >>
                     </a>
-            </div>
+                </div>
 
-             <div className="event-creator-container">
-                    <h2 className="event-container-header">Creators</h2>
-                        <ul className="event-creators-list">
-                            {creator}
+                <div className="event-character-container">
+                    <h2 className="event-container-header">Characters In This Event</h2>
+                        <ul className="event-character-list">
+                            {character}
                         </ul>
-            </div>
-        </section>
-    );
-}
+                </div>
 
+                <div className="event-comic-container">
+                    <h2 className="event-container-header">{this.props.eventResult.title} Core Issues & Tie-In Issues</h2>
+                        <ul className="event-comic-list">
+                            {comic}
+                        </ul>
+                        <a 
+                            href={urls[0]} 
+                            target="_blank"
+                            className="new-event-link"
+                            rel="noopener noreferrer">
+                            See More >>
+                        </a>
+                </div>
+
+                <div className="event-creator-container">
+                        <h2 className="event-container-header">Creators</h2>
+                            <ul className="event-creators-list">
+                                {creator}
+                            </ul>
+                </div>
+            </section>
+        );
+    }
+}
 const mapStateToProps = state => ({
     eventResult: state.eventReducer.clickedEvent,
     eventCharacter: state.eventCharacterReducer.eventCharacter,
