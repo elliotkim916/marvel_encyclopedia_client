0e98d7154108f9f1e74c9b8f20622f1379ebccf8
Elliot Kim <elliotkim916@gmail.com>
2018-05-23 12:38:49 -0700

    established onSubmit so now user can submit notes from anywhere user can take notes

--- src/components/notes-drawer.js
+++ src/components/notes-drawer.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import './notes-drawer.css';
+import {addNotesData} from '../actions/notes';
 
-export default class NotesDrawer extends React.Component {
+export class NotesDrawer extends React.Component {
+    onSubmit(event) {
+        event.preventDefault();
+        if (event.target.noteName.value || event.target.theNotes.value) {
+            this.props.dispatch(addNotesData(
+                event.target.noteName.value,
+                event.target.theNotes.value,
+                this.props.loggedIn.username
+            ));
+        }
+        event.target.noteName.value = '';
+        event.target.theNotes.value = '';
+    }
+    
     render() {
         const closeNav = () => {
             document.getElementById("mySidenav").style.width = "0px";
@@ -14,14 +29,19 @@ export default class NotesDrawer extends React.Component {
                 <div className="notes-explainer">
                     <p className="notes-explainer-text">Take notes on your favorite Marvel characters! Write who they are, what comic books they were in, what comic events & crossovers they were a part of, & any other details you'd like to remember!</p>
                 </div>
-                <form className="notes-form">
+                <form 
+                    className="notes-form"
+                    onSubmit={event => this.onSubmit(event)}    
+                >
                     <input 
                         type="text" 
                         className="notes-title"
-                        placeholder="Enter Note Title" 
+                        placeholder="Enter Note Title"
+                        name="noteName" 
                     />
                     <textarea 
                         className ="users-notes"
+                        name="theNotes"
                         rows="30"
                         cols="18"
                         placeholder="i.e. Thor was a member of the Avengers and his greatest weapon was his hammer Mjolnir!  His brother is Loki but Loki is not Asgardian..." 
@@ -37,3 +57,9 @@ export default class NotesDrawer extends React.Component {
         );
     }
 }
+
+const mapStateToProps = state => ({
+    loggedIn: state.auth.currentUser
+});
+
+export default connect(mapStateToProps)(NotesDrawer);
