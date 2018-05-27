a3b9d0f145184cc28109d3d91db48fa9ca8528b9
Elliot Kim <elliotkim916@gmail.com>
2018-05-26 18:00:28 -0700

    made responsive, gave classNames to name, description, and issue information

--- src/components/comic.js
+++ src/components/comic.js
@@ -19,7 +19,6 @@ export class Comic extends React.Component {
         let imgUrl = '';
         let urls = '';
         let character = '';
-        let creator = '';
 
         if (this.props.comicResult) {
             title = this.props.comicResult.title;
@@ -37,77 +36,66 @@ export class Comic extends React.Component {
 
             if (this.props.comicCharacter) {
                 character = this.props.comicCharacter.map((character, index) => 
-                    <li 
+                    <div 
                         key={index} 
                         onClick={() => this.props.dispatch(searchCharacter(character.name))} 
-                        className="character-name"
+                        className="marvelComicsCharacter"
                     >
                         <img 
                             src={`${character.thumbnail.path.slice(5)}/portrait_fantastic.${character.thumbnail.extension}`} 
-                            alt="character cover"
+                            alt=""
+                            className="cover-art"
                         /><br/>    
-                        {character.name}
-                    </li>
-                );
-            }
-
-            if (this.props.comicResult.creators) {
-                creator = this.props.comicResult.creators.items.map((person, index) => 
-                    <li key={index}>
-                        <span>{person.name}, {person.role.charAt(0).toUpperCase() + person.role.slice(1)}</span>
-                    </li>
+                        <h3 className="characterName">{character.name}</h3>
+                    </div>
                 );
             }
         }
         
         return (
             <section className="comic-section">
-                <a 
-                    href="dashboard"
-                    className="back-home-from-comic"
-                >
-                Home
-                </a>
-                <a 
-                    href="login"
-                    className="log-out-from-comic"
-                    onClick={() => this.logOut}
-                >
-                Log Out
-                </a>
-                <header>
-                    <img src={imgUrl} alt="Comic book cover"/>
-                    <h2>{title}</h2>
-                    <h3><span className="issue-number">Issue Number: {issueNumber}</span><span className="total-pages">Total Pages: {pageCount}</span></h3>
-                </header>
-
-                <div className="comic-description">
-                    <div dangerouslySetInnerHTML={{__html:description}} className="description-container"></div>
-                    {/* <div className="description-container">{description}</div> */}
-                    <div className="character-container">
-                        <h2 className="container-header">Characters In This Issue</h2>
-                            <ul className="character-list">
-                                {character}
-                            </ul>
-                    </div>
-
-                    <div className="creator-container">
-                        <h2 className="container-header">Creators</h2>
-                            <ul>
-                                {creator}
-                            </ul>
+                <div className="comic-book-information">
+                    <a 
+                        href="dashboard"
+                        className="back-home-from-comic"
+                    >
+                    Home
+                    </a>
+                    <a 
+                        href="/"
+                        className="log-out-from-comic"
+                        onClick={() => this.logOut()}
+                    >
+                    Log Out
+                    </a>
+                    
+                    <img 
+                        src={imgUrl} 
+                        alt="Comic book cover"
+                        className="cover-art"    
+                    />
+                    <div className="comicIssueDetails">
+                        <h2 className="comicBookTitle">{title}</h2>
+                        <h3 className="issueInformation"><span className="issue-number">ISSUE #{issueNumber}</span><span className="total-pages">TOTAL PAGES: {pageCount}</span></h3>
+                        <div dangerouslySetInnerHTML={{__html:description}} className="description-container"></div>
+                        <div className="new-comic-link">
+                            <a 
+                                href={urls[0]} 
+                                target="_blank"
+                                className="new-link"
+                                rel="noopener noreferrer">
+                                Purchase Comic<span className="arrows"> >></span>
+                            </a>
+                        </div>
                     </div>
                 </div>
 
-                <div className="comic-links">
-                    <a 
-                        href={urls[0]} 
-                        target="_blank"
-                        className="new-comic-link"
-                        rel="noopener noreferrer">
-                        Purchase Comic >>
-                    </a>
-                </div>         
+                <div className="character-container">
+                    <h2 className="container-header">CHARACTERS IN THIS ISSUE</h2>
+                        <ul className="character-list">
+                            {character}
+                        </ul>
+                </div>    
             </section>
         );
     }
@@ -115,7 +103,8 @@ export class Comic extends React.Component {
 
 const mapStateToProps = state => ({
     comicResult: state.comicReducer.clickedComic,
-    comicCharacter: state.comicCharacterReducer.comicCharacter
+    comicCharacter: state.comicCharacterReducer.comicCharacter,
+    loggedIn: state.auth.currentUser !== null
 });
 
 export default connect(mapStateToProps)(Comic);
