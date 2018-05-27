bf903472c362099303ac4f00ff02825346e856c9
Elliot Kim <elliotkim916@gmail.com>
2018-05-26 18:35:23 -0700

    edited img alt, gave classNames to headers, added Link

--- src/components/registration-page.js
+++ src/components/registration-page.js
@@ -14,12 +14,17 @@ export function RegistrationPage(props) {
         <div className="login-page">
             <header>
                 <h2 className="marvel-header">
-                    <Link to="/" className="marvel-header">Marvel Encyclopedia</Link>
+                    <Link to="/" className="marvel-header">
+                        <img 
+                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/MarvelLogo.svg/2000px-MarvelLogo.svg.png"
+                            alt="Marvel Encyclopedia"
+                            className="register-logo"
+                            /> <span className="second-word">ENCYCLOPEDIA</span></Link>
                 </h2>
             </header>
-            <h3>Begin your Marvel journey now!</h3>
+            <h3 className="second-header">Begin your Marvel journey now!</h3>
             <RegistrationForm /><br/>
-            <span>Already have an account? <Link to="/login">Click here</Link></span>
+            <span className="existing-acct">Already have an account? <Link to="/login" className="click-here">Click here</Link></span>
         </div>
     );
 }
