7d2c9edefb7fb77a082627b248404ba28121c903
Elliot Kim <elliotkim916@gmail.com>
2018-05-28 02:52:44 -0700

    imported icons from image folder to use these images instead

--- src/components/info-section.js
+++ src/components/info-section.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import avengers_icon from '../images/avengers_icon.png';
+import captainAmerica_icon from '../images/captainAmerica_icon.png';
+import captainmarvel_icon from '../images/captainmarvel_icon.png';
 import './info-section.css';
 
 export default function InfoSection(props) {
@@ -26,29 +29,31 @@ export default function InfoSection(props) {
 
             <div className="info explain">
                 <img 
-                    src="https://lumiere-a.akamaihd.net/v1/images/usa_spider-man_chi_kid_arachnid_n_82f070c1.png?region=0%2C0%2C300%2C300" 
+                    src={captainAmerica_icon} 
                     alt="" 
                 />
-                <p className="info-section-text">New to the Marvel Cinematic Universe?  Saw a new Marvel show and have no idea who that character is?  Marvel Encyclopedia allows you to go deep and learn about any Marvel Comics character with biographies, pictures, & issues of comic books the character was in.</p>
+                <p className="info-section-text">Marvel Encyclopedia allows you to go deep and learn about any Marvel Comics character with biographies, pictures, & issues of comic books the character was in.</p>
             </div>
       
             <div className="info explain">
                 <img
-                    src="https://static1.squarespace.com/static/51b3dc8ee4b051b96ceb10de/t/5a8c709253450a0e67a1a041/1519153307713/marvel-comics-reveals-their-new-avengers-superhero-roster2?format=750w"
+                    src={avengers_icon}
                     alt=""
-                    className="comic-front-cover"
                 />
-                <p className="info-section-text">With every character search, Marvel Encyclopedia provides you with the various comic book issues the character was in.  You can save which issues you want to read in the future and save which issues you have already read.</p>
+                <p className="info-section-text">With every character search result, you can save the comics you want to read in the future & save the comics you have already read to your profile page.</p>
             </div>
     
             <div className="info explain">
                 <img
-                    src="https://i.annihil.us/u/prod/marvel/i/mg/b/40/54adba004fe21.png"
+                    src={captainmarvel_icon}
                     alt=""
-                    className="dr-strange-img"
                 />
-                <p className="info-section-text">Every Marvel character has a rich history and is involved in many stories.  Click on a comic or an event to learn more about the story, to see the characters that were involved, and see which comic book issues tie in with that particular event.</p>
+                <p className="info-section-text">Click on a comic or an event to learn more about the story, to see the characters that were involved, and see which comic book issues tie in with that particular event.</p>
             </div>
         </section>
     );
 }
+
+// "https://i.annihil.us/u/prod/marvel/i/mg/b/40/54adba004fe21.png"
+// "https://static1.squarespace.com/static/51b3dc8ee4b051b96ceb10de/t/5a8c709253450a0e67a1a041/1519153307713/marvel-comics-reveals-their-new-avengers-superhero-roster2?format=750w"
+// "https://lumiere-a.akamaihd.net/v1/images/usa_spider-man_chi_kid_arachnid_n_82f070c1.png?region=0%2C0%2C300%2C300"
