526a677b9478747cdf5b2f2a4314472e77f023a2
Elliot Kim <elliotkim916@gmail.com>
2018-05-28 13:53:31 -0700

    cleaned up code to have 3 reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,29 +1,25 @@
 import {
     SEARCH_CHARACTER_REQUEST,
     SEARCH_CHARACTER_SUCCESS,
-    SEARCH_CHARACTER_ERROR,
+    SEARCH_CHARACTER_COMIC_SUCCESS,
+    SEARCH_CHARACTER_EVENT_SUCCESS,
+    SEARCH_CHARACTER_ERROR
+} from '../actions/characters';
+
+import {
     FIND_COMIC_REQUEST,
     FIND_COMIC_SUCCESS,
-    FIND_COMIC_ERROR,
+    FIND_COMIC_CHARACTER_SUCCESS,
+    FIND_COMIC_ERROR
+} from '../actions/comics';
+
+import {
     FIND_EVENT_REQUEST,
     FIND_EVENT_SUCCESS,
-    FIND_EVENT_ERROR,
-    SEARCH_CHARACTER_COMIC_REQUEST,
-    SEARCH_CHARACTER_COMIC_SUCCESS,
-    SEARCH_CHARACTER_COMIC_ERROR,
-    SEARCH_CHARACTER_EVENT_REQUEST,
-    SEARCH_CHARACTER_EVENT_SUCCESS,
-    SEARCH_CHARACTER_EVENT_ERROR,
-    FIND_COMIC_CHARACTER_REQUEST,
-    FIND_COMIC_CHARACTER_SUCCESS,
-    FIND_COMIC_CHARACTER_ERROR,
-    SEARCH_EVENT_CHARACTER_REQUEST,
     SEARCH_EVENT_CHARACTER_SUCCESS,
-    SEARCH_EVENT_CHARACTER_ERROR,
-    SEARCH_EVENT_COMIC_REQUEST,
     SEARCH_EVENT_COMIC_SUCCESS,
-    SEARCH_EVENT_COMIC_ERROR
-} from '../actions';
+    FIND_EVENT_ERROR
+} from '../actions/events';
 
 const initialState = {
     searchedCharacter: {},
@@ -38,111 +34,6 @@ const initialState = {
     error: null
 };
 
-export function eventComicReducer(state=initialState, action) {
-    if (action.type === SEARCH_EVENT_COMIC_REQUEST) {
-        return Object.assign({}, state, {
-            loading: true,
-            error: null
-        });
-    } else if (action.type === SEARCH_EVENT_COMIC_SUCCESS) {
-        return Object.assign({}, state, {
-            eventComic: action.eventComic,
-            loading: false,
-            error: null
-        });
-    } else if (action.type === SEARCH_EVENT_COMIC_ERROR) {
-        return Object.assign({}, state, {
-            error: action.error,
-            loading: false
-        });
-    }
-    return state;
-}
-
-export function eventCharacterReducer(state=initialState, action) {
-    if (action.type === SEARCH_EVENT_CHARACTER_REQUEST) {
-        return Object.assign({}, state, {
-            loading: true,
-            error: null
-        });
-    } else if (action.type === SEARCH_EVENT_CHARACTER_SUCCESS) {
-        return Object.assign({}, state, {
-            eventCharacter: action.eventCharacter,
-            loading: false,
-            error: null
-        });
-    } else if (action.type === SEARCH_EVENT_CHARACTER_ERROR) {
-        return Object.assign({}, state, {
-            error: action.error,
-            loading: false
-        });
-    }
-    return state;
-}
-
-export function characterEventReducer(state=initialState, action) {
-    if (action.type === SEARCH_CHARACTER_EVENT_REQUEST) {
-        return Object.assign({}, state, {
-            loading: true,
-            error: null
-        });
-    } else if (action.type === SEARCH_CHARACTER_EVENT_SUCCESS) {
-        return Object.assign({}, state, {
-            searchedCharacterEvent: action.characterEvent,
-            loading: false,
-            error: null
-        });
-    } else if (action.type === SEARCH_CHARACTER_EVENT_ERROR) {
-        return Object.assign({}, state, {
-            error: action.error,
-            loading: false
-        });
-    }
-    return state;
-}
-
-export function comicCharacterReducer(state=initialState, action) {
-    if (action.type === FIND_COMIC_CHARACTER_REQUEST) {
-        return Object.assign({}, state, {
-            loading: true,
-            error: null
-        });
-    } else if (action.type === FIND_COMIC_CHARACTER_SUCCESS) {
-        return Object.assign({}, state, {
-            comicCharacter: action.comicCharacter,
-            loading: false,
-            error: null
-        });
-    } else if (action.type === FIND_COMIC_CHARACTER_ERROR) {
-        return Object.assign({}, state, {
-            error: action.error,
-            loading: false
-        });
-    }
-    return state;
-}
-
-export function characterComicReducer(state=initialState, action) {
-    if (action.type === SEARCH_CHARACTER_COMIC_REQUEST) {
-        return Object.assign({}, state, {
-            loading: true,
-            error: null
-        });
-    } else if (action.type === SEARCH_CHARACTER_COMIC_SUCCESS) {
-        return Object.assign({}, state, {
-            searchedCharacterComic: action.characterComic,
-            loading: false,
-            error: null
-        });
-    } else if (action.type === SEARCH_CHARACTER_COMIC_ERROR) {
-        return Object.assign({}, state, {
-            error: action.error,
-            loading: false
-        });
-    }
-    return state;
-}
-
 export function characterReducer(state=initialState, action) {
     if (action.type === SEARCH_CHARACTER_REQUEST) {
         return Object.assign({}, state, {
@@ -155,6 +46,18 @@ export function characterReducer(state=initialState, action) {
             loading: false,
             error: null
         });
+    } else if (action.type === SEARCH_CHARACTER_COMIC_SUCCESS) {
+        return Object.assign({}, state, {
+            searchedCharacterComic: action.characterComic,
+            loading: false,
+            error: null
+        });
+    } else if (action.type === SEARCH_CHARACTER_EVENT_SUCCESS) {
+        return Object.assign({}, state, {
+            searchedCharacterEvent: action.characterEvent,
+            loading: false,
+            error: null
+        }); 
     } else if (action.type === SEARCH_CHARACTER_ERROR) {
         return Object.assign({}, state, {
             error: action.error,
@@ -176,6 +79,12 @@ export function comicReducer(state=initialState, action) {
             loading: false,
             error: null
         });
+    } else if (action.type === FIND_COMIC_CHARACTER_SUCCESS) {
+        return Object.assign({}, state, {
+            comicCharacter: action.comicCharacter,
+            loading: false,
+            error: null
+        });
     } else if (action.type === FIND_COMIC_ERROR) {
         return Object.assign({}, state, {
             error: action.error,
@@ -194,6 +103,18 @@ export function eventReducer(state=initialState, action) {
     } else if (action.type === FIND_EVENT_SUCCESS) {
         return Object.assign({}, state, {
             clickedEvent: action.event,
+            loading: false,
+            error: null
+        });
+    } else if (action.type === SEARCH_EVENT_CHARACTER_SUCCESS) {
+        return Object.assign({}, state, {
+            eventCharacter: action.eventCharacter,
+            loading: false,
+            error: null
+        }); 
+    } else if (action.type === SEARCH_EVENT_COMIC_SUCCESS) {
+        return Object.assign({}, state, {
+            eventComic: action.eventComic,
             loading: false,
             error: null
         });
