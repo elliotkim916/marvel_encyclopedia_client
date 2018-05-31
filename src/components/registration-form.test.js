001d444bbdb406ff828f37fdb93cf8341f2fce69
Elliot Kim <elliotkim916@gmail.com>
2018-05-31 11:19:26 -0700

    added test for registration-form.js

---
+++ src/components/registration-form.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+
+import RegistrationForm from './registration-form';
+
+describe('<RegistrationForm />', () => {
+    it('Renders without crashing', () => {
+        shallow(<RegistrationForm />);
+    });
+});
