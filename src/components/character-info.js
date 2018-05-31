59388222f8deced0d1594cf66264b328452654e4
Elliot Kim <elliotkim916@gmail.com>
2018-05-31 11:34:24 -0700

    added loading conditional statement with mapStateToProps

--- src/components/character-info.js
+++ src/components/character-info.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import './character-info.css'
 
-export default class CharacterInfo extends React.Component {
-    render() {
+export class CharacterInfo extends React.Component {
+    renderResults() {
+        if (this.props.loading) {
+            return <div className="loading">L O A D I N G . . . </div>
+        }
+
         let imgUrl = '';
         if (this.props.character.thumbnail) {
             imgUrl = this.props.character.thumbnail.path.slice(5) + '/portrait_uncanny.' + this.props.character.thumbnail.extension;
@@ -14,29 +19,42 @@ export default class CharacterInfo extends React.Component {
         }
 
         return (
-            <section className="character-info-section">
-                <div>
-                    <img 
-                        src={imgUrl}
-                        alt="" 
-                        className="searched-img" 
-                    />
-                    <div className="character-description">
-                        <h2 className="character-name">{this.props.character.name}</h2>
-                        <p className="character-text">{this.props.character.description}</p>
-                        <div className="info-new-link">
-                            <a 
-                                href={urls[1]}
-                                target="_blank"
-                                className="new-link"
-                                rel="noopener noreferrer">
-                                Read More<span className="character-arrows"> >></span>
-                            </a>
-                        </div>
+            <div>
+                <img 
+                    src={imgUrl}
+                    alt="" 
+                    className="searched-img" 
+                />
+                <div className="character-description">
+                    <h2 className="character-name">{this.props.character.name}</h2>
+                    <p className="character-text">{this.props.character.description}</p>
+                    <div className="info-new-link">
+                        <a 
+                            href={urls[1]}
+                            target="_blank"
+                            className="new-link"
+                            rel="noopener noreferrer">
+                            Read More<span className="character-arrows"> >></span>
+                        </a>
                     </div>
                 </div>
-            </section>
+            </div>
         );
     }
-}
+        
+    render() {
+        return (
+            <section className="character-info-section">
+            <hr className="style10"/>
+               {this.renderResults()}
+            </section>
+            );
+        }
+    }
+
+const mapStateToProps = state => ({
+    loading: state.characterReducer.characterLoading
+});
+
+export default connect(mapStateToProps)(CharacterInfo);
 
