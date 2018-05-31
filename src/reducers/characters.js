0a270ee50b517ac4a3bc9bc3bee2d9c113a7f8f2
Elliot Kim <elliotkim916@gmail.com>
2018-05-31 11:29:11 -0700

    importing new request functions, along with establishing 3 different loading states in initialState

--- src/reducers/characters.js
+++ src/reducers/characters.js
@@ -1,7 +1,9 @@
 import {
     SEARCH_CHARACTER_REQUEST,
     SEARCH_CHARACTER_SUCCESS,
+    SEARCH_CHARACTER_COMIC_REQUEST,
     SEARCH_CHARACTER_COMIC_SUCCESS,
+    SEARCH_CHARACTER_EVENT_REQUEST,
     SEARCH_CHARACTER_EVENT_SUCCESS,
     SEARCH_CHARACTER_ERROR
 } from '../actions/characters';
@@ -10,32 +12,44 @@ const initialState = {
     searchedCharacter: {},
     searchedCharacterComic: [],
     searchedCharacterEvent: [],
-    loading: false,
+    characterloading: false,
+    comicLoading: false,
+    eventLoading: false,
     error: null
 };
 
 export function characterReducer(state=initialState, action) {
     if (action.type === SEARCH_CHARACTER_REQUEST) {
         return Object.assign({}, state, {
-            loading: true,
+            characterLoading: true,
             error: null
         });
-    } else if (action.type === SEARCH_CHARACTER_SUCCESS) {
+    } else if (action.type === SEARCH_CHARACTER_COMIC_REQUEST) {
+        return Object.assign({}, state, {
+            comicLoading: true,
+            error: null
+        });
+    } else if (action.type === SEARCH_CHARACTER_EVENT_REQUEST) {
+        return Object.assign({}, state, {
+            eventLoading: true,
+            error: null
+        });
+    }  else if (action.type === SEARCH_CHARACTER_SUCCESS) {
         return Object.assign({}, state, {
             searchedCharacter: action.character,
-            loading: false,
+            characterLoading: false,
             error: null
         });
     } else if (action.type === SEARCH_CHARACTER_COMIC_SUCCESS) {
         return Object.assign({}, state, {
             searchedCharacterComic: action.characterComic,
-            loading: false,
+            comicLoading: false,
             error: null
         });
     } else if (action.type === SEARCH_CHARACTER_EVENT_SUCCESS) {
         return Object.assign({}, state, {
             searchedCharacterEvent: action.characterEvent,
-            loading: false,
+            eventLoading: false,
             error: null
         }); 
     } else if (action.type === SEARCH_CHARACTER_ERROR) {
