1152d6247f44bc841466e4b602bb22a8f5251ecb
Elliot Kim <elliotkim916@gmail.com>
2018-05-31 13:23:50 -0700

    cleaned up code

--- src/components/search-form.js
+++ src/components/search-form.js
@@ -19,14 +19,14 @@ export class SearchForm extends React.Component {
         return (
             <section className="search-form-section">
                 <header className="search-form-header">
-                    <h1 className="form-header-text">
-                        <img 
-                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/MarvelLogo.svg/2000px-MarvelLogo.svg.png" 
-                            alt="Marvel Encyclopedia"
-                            className="marvel-logo"
-                        /> 
-                        <div className="header-text">ENCYCLOPEDIA</div>
-                    </h1>
+                        <h1 className="form-header-text">
+                            <img 
+                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/MarvelLogo.svg/2000px-MarvelLogo.svg.png" 
+                                alt="Marvel Encyclopedia"
+                                className="marvel-logo"
+                            /> 
+                            <div className="header-text">ENCYCLOPEDIA</div>
+                        </h1>
                 </header>
                 <form 
                     className="search-form" 
