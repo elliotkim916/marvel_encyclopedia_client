e15d6851e47fcb01c550b66b5066da1a0882131d
Elliot Kim <elliotkim916@gmail.com>
2018-05-31 11:24:39 -0700

    added test for comics.js

---
+++ src/reducers/comics.test.js
@@ -0,0 +1,17 @@
+import {comicReducer} from './comics';
+
+describe('comicReducer', () => {
+    it('Should set the initial state when nothing is passed in', () => {
+        const state = comicReducer(undefined, {type: '__UNKNOWN'});
+        expect(state).toHaveProperty('clickedComic', {});
+        expect(state).toHaveProperty('comicCharacter', []);
+        expect(state).toHaveProperty('loading', false);
+        expect(state).toHaveProperty('error', null);
+    });
+
+    it('Should return the current state on an unknown action', () => {
+        let currentState = {};
+        const state = comicReducer(currentState, {type: '__UNKNOWN'});
+        expect(state).toBe(currentState);
+    });
+});
