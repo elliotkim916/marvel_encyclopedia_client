c3b90e3237ed5248bc98495075a11b3aa134b66f
Elliot Kim <elliotkim916@gmail.com>
2018-05-31 11:19:45 -0700

    added test for registration-page.js

---
+++ src/components/registration-page.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import {RegistrationPage} from './registration-page';
+
+describe(<RegistrationPage />, () => {
+    it('Renders without crashing', () => {
+        shallow(<RegistrationPage />);
+    });
+});
