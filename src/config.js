5cf7c70382c6c4359332b66eeceb4d9a93fca233
Elliot Kim <elliotkim916@gmail.com>
2018-05-31 18:58:10 -0700

    updated api base url

--- src/config.js
+++ src/config.js
@@ -1,11 +1,8 @@
-export const MARVEL_API_BASE_URL = "//gateway.marvel.com:443/v1/";
+export const MARVEL_API_BASE_URL = "https://gateway.marvel.com:443/v1/";
 export const MARVEL_CHARACTERS_ENDPOINT = "public/characters";
 export const PUBLIC_KEY = "fd39c1a1e2bfae2ff6b93140da9570c0";
 export const TS = "2018-05-07T05:25:16.227Z";
 export const HASH = "06629b5ece79a47a43298a7ebc99af21";
 
 // server-side
-export const API_BASE_URL = 
-    process.env.REACT_APP_API_BASE_URL || '//polar-waters-95107.herokuapp.com/api';
-
-   
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://polar-waters-95107.herokuapp.com/api';
