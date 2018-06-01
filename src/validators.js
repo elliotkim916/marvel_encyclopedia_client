fc37a0968f3438068f1888e602b11046eef81233
Elliot Kim <elliotkim916@gmail.com>
2018-06-01 00:17:44 -0700

    cleaned up code

--- src/validators.js
+++ src/validators.js
@@ -13,13 +13,6 @@ export const valueLength = value => {
     if (value.length > 25) {
         return `Must be at most 25 characters long`;
     }
-    // console.log(length, value);
-    // if (length.min && value.length < length.min) {
-    //     return `Must be at least ${length.min} characters long`;
-    // }
-    // if (length.max && value.length > length.max) {
-    //     return `Must be at most ${length.max} characters long`;
-    // }
 };
 
 // allValues is an object which contains the values entered into each form
