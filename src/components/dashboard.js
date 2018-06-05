3fe2d21dffbece56e6d04da5516c29379790a18f
Elliot Kim <elliotkim916@gmail.com>
2018-06-04 23:01:21 -0700

    updated onDelete function

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -27,7 +27,7 @@ export class Dashboard extends React.Component {
         const result = window.confirm('Are you sure you want to delete?  If so, click OK.');
         if (result) {
             this.props.dispatch(deleteData(id))
-            window.location.reload();
+            .then(data => window.location.reload());
         } else {
             e.target.blur();
         }
