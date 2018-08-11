d3c1ded24c9b170877dce78975be8800f474b93d
Elliot Kim <elliotkim916@gmail.com>
2018-08-11 16:01:34 -0700

    added spinner and message when user is logging in and/or creating account while it is still loading

--- src/components/login-page.js
+++ src/components/login-page.js
@@ -6,35 +6,44 @@ import LoginForm from './login-form';
 import './registration-page.css';
 
 export function LoginPage(props) {
-    if (props.loggedIn) {
-        return <Redirect to="/dashboard" />;
-    }
+	if (props.loading) {
+		return 	<div className="account_loader">
+							<h3 className="loading_message">Logging in..</h3>
+							<div className="loader"></div>
+						</div>
+	}
 
-    return (
-        <div className="login-page">
-            <header>
-                <h2>
-                    <Link to="/" className="marvel-header">    
-                    <img 
-                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/MarvelLogo.svg/2000px-MarvelLogo.svg.png"
-                        alt="Marvel Encyclopedia"
-                        className="register-logo"
-                        /> <span className="second-word">ENCYCLOPEDIA</span></Link>
-                </h2>
-            </header>
-            <LoginForm />
-            <span className="register-redirect">Don't have an account? <Link to="/register" className="click-here">Sign up here</Link></span>
-            <div className="demo-container">
-                <h3>Demo Account</h3>
-                <p className="demo-username">Username: demouser</p>
-                <p className="demo-password">Password: demopassword</p>
-            </div>
-        </div>
-    )
+	if (props.loggedIn) {
+		return <Redirect to="/dashboard" />;
+	}
+
+	return (
+		<div className="login-page">
+			<header>
+				<h2>
+					<Link to="/" className="marvel-header">    
+					<img 
+							src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/MarvelLogo.svg/2000px-MarvelLogo.svg.png"
+							alt="Marvel Encyclopedia"
+							className="register-logo"
+							/> <span className="second-word">ENCYCLOPEDIA</span>
+					</Link>
+				</h2>
+			</header>
+			<LoginForm />
+			<span className="register-redirect">Don't have an account? <Link to="/register" className="click-here">Sign up here</Link></span>
+			<div className="demo-container">
+				<h3>Demo Account</h3>
+				<p className="demo-username">Username: demouser</p>
+				<p className="demo-password">Password: demopassword</p>
+			</div>
+		</div>
+	)
 }
 
 const mapStateToProps = state => ({
-    loggedIn: state.auth.currentUser !== null
+	loggedIn: state.auth.currentUser !== null,
+	loading: state.auth.loading
 });
 
 export default connect(mapStateToProps)(LoginPage);
--- src/components/registration-page.js
+++ src/components/registration-page.js
@@ -6,31 +6,41 @@ import RegistrationForm from './registration-form';
 import './registration-page.css';
 
 export function RegistrationPage(props) {
+    if (props.loading) {
+		return 	<div className="account_loader">
+							<h3 className="loading_message">Creating account..</h3>
+							<div className="loader"></div>
+						</div>
+    }
+    
     if (props.loggedIn) {
-        return <Redirect to="/dashboard" />;
+      return <Redirect to="/dashboard" />;
     }
 
     return (
-        <div className="login-page">
-            <header>
-                <h2 className="marvel-header">
-                    <Link to="/" className="marvel-header">
-                        <img 
-                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/MarvelLogo.svg/2000px-MarvelLogo.svg.png"
-                            alt="Marvel Encyclopedia"
-                            className="register-logo"
-                            /> <span className="second-word">ENCYCLOPEDIA</span></Link>
-                </h2>
-            </header>
-            <h3 className="second-header">Begin your Marvel journey now!</h3>
-            <RegistrationForm /><br/>
-            <span className="existing-acct">Already have an account? <Link to="/login" className="click-here">Click here</Link></span>
-        </div>
-    );
+			<div className="login-page">
+				<header>
+					<h2 className="marvel-header">
+						<Link to="/" className="marvel-header">
+							<img 
+								src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/MarvelLogo.svg/2000px-MarvelLogo.svg.png"
+								alt="Marvel Encyclopedia"
+								className="register-logo"
+								/> 
+								<span className="second-word">ENCYCLOPEDIA</span>
+						</Link>
+					</h2>
+				</header>
+				<h3 className="second-header">Begin your Marvel journey now!</h3>
+				<RegistrationForm /><br/>
+				<span className="existing-acct">Already have an account? <Link to="/login" className="click-here">Click here</Link></span>
+		</div>
+	);
 }
 
 const mapStateToProps = state => ({
-    loggedIn: state.auth.currentUser !== null
+	loggedIn: state.auth.currentUser !== null,
+	loading: state.auth.loading
 });
 
 export default connect(mapStateToProps)(RegistrationPage);
