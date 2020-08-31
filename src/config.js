ed3d4d42a0d11e277a07b1cd73fdcb53321ca28e
Elliot Kim <elliotkim916@gmail.com>
2020-08-30 18:46:43 -0700

    Updated packages

--- src/config.js
+++ src/config.js
@@ -1,8 +1,10 @@
-export const MARVEL_API_BASE_URL = "https://gateway.marvel.com:443/v1/";
-export const MARVEL_CHARACTERS_ENDPOINT = "public/characters";
-export const PUBLIC_KEY = "fd39c1a1e2bfae2ff6b93140da9570c0";
-export const TS = "2018-05-07T05:25:16.227Z";
-export const HASH = "06629b5ece79a47a43298a7ebc99af21";
+export const MARVEL_API_BASE_URL = 'https://gateway.marvel.com:443/v1/';
+export const MARVEL_CHARACTERS_ENDPOINT = 'public/characters';
+export const PUBLIC_KEY = 'fd39c1a1e2bfae2ff6b93140da9570c0';
+export const TS = '2018-05-07T05:25:16.227Z';
+export const HASH = '06629b5ece79a47a43298a7ebc99af21';
 
 // server-side
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
+export const API_BASE_URL =
+  // process.env.REACT_APP_API_BASE_URL ||
+  'http://localhost:8080/api';
