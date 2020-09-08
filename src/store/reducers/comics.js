5b55cf72cdd7b6010a1a5e493ab42ac27fc476af
Elliot Kim <elliotkim916@gmail.com>
2020-09-08 13:11:50 -0700

    Updated comics reducer

--- src/store/reducers/comics.js
+++ src/store/reducers/comics.js
@@ -3,44 +3,49 @@ import {
   FIND_COMIC_SUCCESS,
   FIND_COMIC_CHARACTER_REQUEST,
   FIND_COMIC_CHARACTER_SUCCESS,
-  FIND_COMIC_ERROR
+  FIND_COMIC_ERROR,
 } from '../actions/comics';
 
 const initialState = {
   clickedComic: {},
   comicCharacter: [],
   loading: false,
-  error: null
-}
+  error: null,
+};
 
-export function comicReducer(state=initialState, action) {
-  if (action.type === FIND_COMIC_REQUEST) {
-    return Object.assign({}, state, {
-      loading: true,
-      error: null
-    });
-  } else if (action.type === FIND_COMIC_CHARACTER_REQUEST) {
-    return Object.assign({}, state, {
-      loading: true,
-      error: null
-    });
-  } else if (action.type === FIND_COMIC_SUCCESS) {
-    return Object.assign({}, state, {
-      clickedComic: action.comic,
-      loading: false,
-      error: null
-    });
-  } else if (action.type === FIND_COMIC_CHARACTER_SUCCESS) {
-    return Object.assign({}, state, {
-      comicCharacter: action.comicCharacter,
-      loading: false,
-      error: null
-    });
-  } else if (action.type === FIND_COMIC_ERROR) {
-    return Object.assign({}, state, {
-      error: action.error,
-      loading: false
-    });
+const findComicRequest = (state, action) => {
+  return { ...state, loading: true };
+};
+
+const findComicCharacterRequest = (state, action) => {
+  return { ...state, loading: true };
+};
+
+const findComicSuccess = (state, action) => {
+  return { ...state, clickedComic: action.comic, loading: false };
+};
+
+const findComicCharacterSuccess = (state, action) => {
+  return { ...state, comicCharacter: action.comicCharacter, loading: false };
+};
+
+const findComicError = (state, action) => {
+  return { ...state, error: action.error, loading: false };
+};
+
+export function comicReducer(state = initialState, action) {
+  switch (action.type) {
+    case FIND_COMIC_REQUEST:
+      return findComicRequest(state, action);
+    case FIND_COMIC_CHARACTER_REQUEST:
+      return findComicCharacterRequest(state, action);
+    case FIND_COMIC_SUCCESS:
+      return findComicSuccess(state, action);
+    case FIND_COMIC_CHARACTER_SUCCESS:
+      return findComicCharacterSuccess(state, action);
+    case FIND_COMIC_ERROR:
+      return findComicError(state, action);
+    default:
+      return state;
   }
-  return state;
 }
